4c3ff4fee7589c52bff4f0f7ac9aee05acccadbc
eros-gir <eduard.ros.g@gmail.com>
2025-04-26 15:39:41 +0200

    Still working on grace

--- grace/grace.c
+++ grace/grace.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
+
 #define FILENAME "grace_kid.c"
 #define COMMENT "/* Comentario obligatorio */\n"
 #define GRACE() \
 int main(void) { \
     FILE *f = fopen(FILENAME, "w"); \
-    if (!f) return 1; \
-	char *code = "#include <stdio.h>\\n" \
-	"#define FILENAME \\\"Grace_kid.c\\\"\\n" \
-	"#define COMMENT \\\"/* Comentario obligatorio */\\\\n\\\"\\n" \
-	"#define GRACE() \\\n" \
-	"int main(void) { \\\n" \
-	"    FILE *f = fopen(FILENAME, \\\"w\\\"); \\\n" \
-	"    if (!f) return 1; \\\n" \
-	"    char *code = %c%s%c; \\\n" \
-	"    fprintf(f, code, 34, code, 34); \\\n" \
-	"    return 0; \\\n" \
-	"}\n\n" \
-	"/* Comentario obligatorio */\n" \
-	"GRACE()\n"; \
-    fprintf(f, code, 34, code, 34); \
-    return 0; \
+    if (!f) return (1); \
+    char *code = "#include <stdio.h>%c" \
+"#define FILENAME %cgrace_kid.c%c%c" \
+"#define COMMENT %c/* Comentario obligatorio */\\n%c" \
+"#define GRACE() %c" \
+"int main(void) { %c" \
+"    FILE *f = fopen(FILENAME, %cw%c); %c" \
+"    if (!f) return (1); %c" \
+"    char *code = %c%s%c; %c" \
+"    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); %c" \
+"    fclose(f); %c" \
+"    return (0); %c" \
+"}%c" \
+"COMMENT%c" \
+"GRACE()%c"; \
+    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10, 10); \
+    fclose(f); \
+    return (0); \
 }
 
 GRACE()
