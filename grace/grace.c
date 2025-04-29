bb23bf952203d345b82a5512ba4a1897498ea0bd
Eduard Ros <eduard.ros.g@gmail.com>
2025-04-29 11:47:29 +0200

    Arreglado el primer grace, el grace kid no es reproducible aun

--- grace/grace.c
+++ grace/grace.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
 #define FILENAME "grace_kid.c"
-#define COMMENT "/* Comentario obligatorio */\n"
+#define GRACE_COMMENT "/* Comentario obligatorio */\n"
 #define GRACE() \
 int main(void) { \
     FILE *f = fopen(FILENAME, "w"); \
     if (!f) return (1); \
-    char *code = "#include <stdio.h>%c" \
-"#define FILENAME %cgrace_kid.c%c%c" \
-"#define COMMENT %c/* Comentario obligatorio */\\n%c" \
-"#define GRACE() %c" \
-"int main(void) { %c" \
-"    FILE *f = fopen(FILENAME, %cw%c); %c" \
-"    if (!f) return (1); %c" \
-"    char *code = %c%s%c; %c" \
-"    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); %c" \
-"    fclose(f); %c" \
-"    return (0); %c" \
-"}%c" \
-"COMMENT%c" \
-"GRACE()%c"; \
-    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10, 10); \
+    fputs(GRACE_COMMENT, f); \
+    char *code = "#include <stdio.h>%c \
+#define FILENAME %cgrace_kid.c%c%c \
+#define COMMENT %c/* Comentario obligatorio */\\n%c%c \
+#define GRACE() %c \
+int main(void) { %c \
+    FILE *f = fopen(FILENAME, %cw%c); %c \
+    if (!f) return (1); %c \
+    fputs(GRACE_COMMENT, f); %c \
+    char *code = %c%s%c; %c \
+    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); %c \
+    fclose(f); %c \
+    return (0); %c \
+}%c \
+GRACE()%c"; \
+    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); \
     fclose(f); \
     return (0); \
 }
