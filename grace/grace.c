b0eed4c1ed00d85bd88f36292cad7443fbe6d1f5
Eduard Ros <eduard.ros.g@gmail.com>
2025-05-09 11:52:41 +0200

    Grace almost finished, compiles and runs grace_kid once, but the format is a little wrong

--- grace/grace.c
+++ grace/grace.c
@@ -7,20 +7,20 @@ int main(void) { \
     FILE *f = fopen(FILENAME, "w"); \
     if (!f) return (1); \
     fputs(GRACE_COMMENT, f); \
-    char *code = "#include <stdio.h>%c \
-#define FILENAME %cgrace_kid.c%c%c \
-#define COMMENT %c/* Comentario obligatorio */\\n%c%c \
-#define GRACE() %c \
-int main(void) { %c \
-    FILE *f = fopen(FILENAME, %cw%c); %c \
-    if (!f) return (1); %c \
-    fputs(GRACE_COMMENT, f); %c \
-    char *code = %c%s%c; %c \
-    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); %c \
-    fclose(f); %c \
-    return (0); %c \
-}%c \
-GRACE()%c"; \
+    char *code = "#include <stdio.h>%c" \
+    "#define FILENAME %cgrace_kid.c%c%c" \
+    "#define GRACE_COMMENT %c/* Comentario obligatorio */%c%c" \
+    "#define GRACE() \\%c" \
+    "int main(void) { \\%c" \
+    "    FILE *f = fopen(FILENAME, %cw%c); \\%c" \
+    "    if (!f) return (1); \\%c" \
+    "    fputs(GRACE_COMMENT, f); \\%c" \
+    "    char *code = %c%s%c; \\%c" \
+    "    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); \\%c" \
+    "    fclose(f); \\%c" \
+    "    return (0); \\%c" \
+    "}%c" \
+    "GRACE()%c"; \
     fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); \
     fclose(f); \
     return (0); \
