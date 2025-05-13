fbd690ec34e7eccd14b494badc1ec64eebe8d5cc
Eduard Ros <eduard.ros.g@gmail.com>
2025-05-13 11:58:55 +0200

    casi listo, recuerda que he añadido %c extras y necesitan ser \ en ascii es 92

--- grace/grace.c
+++ grace/grace.c
@@ -10,15 +10,15 @@ int main(void) { \
     char *code = "#include <stdio.h>%c" \
     "#define FILENAME %cgrace_kid.c%c%c" \
     "#define GRACE_COMMENT %c/* Comentario obligatorio */%c%c" \
-    "#define GRACE() \\%c" \
-    "int main(void) { \\%c" \
-    "    FILE *f = fopen(FILENAME, %cw%c); \\%c" \
-    "    if (!f) return (1); \\%c" \
-    "    fputs(GRACE_COMMENT, f); \\%c" \
-    "    char *code = %c%s%c; \\%c" \
-    "    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); \\%c" \
-    "    fclose(f); \\%c" \
-    "    return (0); \\%c" \
+    "#define GRACE() %c%c" \
+    "int main(void) { %c %c" \
+    "    FILE *f = fopen(FILENAME, %cw%c); %c%c" \
+    "    if (!f) return (1); %c%c" \
+    "    fputs(GRACE_COMMENT, f); %c%c" \
+    "    char *code = %c%s%c; %c%c" \
+    "    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); %c%c" \
+    "    fclose(f); %c%c" \
+    "    return (0); %c%c" \
     "}%c" \
     "GRACE()%c"; \
     fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); \
