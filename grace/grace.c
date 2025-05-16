3f0569f0f67548526321041d0f52456ffa58ecf8
Eduard Ros <eduard.ros.g@gmail.com>
2025-05-16 11:13:33 +0200

    Grace finished, and working fine, time to start Sully

--- grace/grace.c
+++ grace/grace.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 #define FILENAME "grace_kid.c"
-#define GRACE_COMMENT "/* Comentario obligatorio */\n"
+#define GRACE_COMMENT "/* Comentario obligatorio */"
 #define GRACE() \
 int main(void) { \
     FILE *f = fopen(FILENAME, "w"); \
     if (!f) return (1); \
     fputs(GRACE_COMMENT, f); \
-    char *code = "#include <stdio.h>%c" \
+    char *code = "%c#include <stdio.h>%c" \
     "#define FILENAME %cgrace_kid.c%c%c" \
     "#define GRACE_COMMENT %c/* Comentario obligatorio */%c%c" \
     "#define GRACE() %c%c" \
-    "int main(void) { %c %c" \
+    "int main(void) { %c%c" \
     "    FILE *f = fopen(FILENAME, %cw%c); %c%c" \
     "    if (!f) return (1); %c%c" \
     "    fputs(GRACE_COMMENT, f); %c%c" \
     "    char *code = %c%s%c; %c%c" \
-    "    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); %c%c" \
+    "        fprintf(f, code, 10, 10, 34, 34, 10, 34, 34, 10, 92, 10, 92, 10, 34, 34, 92, 10, 92, 10, 92, 10, 34, code, 34, 92, 10, 92, 10, 92, 10, 92, 10, 10, 10); %c%c" \
     "    fclose(f); %c%c" \
     "    return (0); %c%c" \
     "}%c" \
     "GRACE()%c"; \
-    fprintf(f, code, 10, 34, 34, 10, 34, 34, 10, 10, 10, 34, 34, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10); \
+    fprintf(f, code, 10, 10, 34, 34, 10, 34, 34, 10, 92, 10, 92, 10, 34, 34, 92, 10, 92, 10, 92, 10, 34, code, 34, 92, 10, 92, 10, 92, 10, 92, 10, 10, 10); \
     fclose(f); \
     return (0); \
 }
