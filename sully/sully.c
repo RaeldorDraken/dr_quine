68dd8e31b178bd1418f3d5603a307451f5703b45
Eduard Ros <eduard.ros.g@gmail.com>
2025-05-28 11:42:38 +0200

    sully aun no funciona

--- sully/sully.c
+++ sully/sully.c
@@ -1,49 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define I 5
-
-#define CODE "#include <stdio.h>\\n"\
-"#include <stdlib.h>\\n\\n"\
-"#define I %d\\n\\n"\
-"#define CODE %c%s%c\\n\\n"\
-"int main(void) {\\n"\
-"    if (I <= 0) return 0;\\n\\n"\
-"    char fname[20];\\n"\
-"    sprintf(fname, \\\"Sully_%%d.c\\\", I - 1);\\n\\n"\
-"    FILE *f = fopen(fname, \\\"w\\\");\\n"\
-"    if (!f) return 1;\\n\\n"\
-"    fprintf(f, CODE, I - 1, 34, CODE, 34);\\n"\
-"    fclose(f);\\n\\n"\
-"    char compile[100];\\n"\
-"    sprintf(compile, \\\"clang -Wall -Wextra -Werror -o Sully_%%d Sully_%%d.c\\\", I - 1, I - 1);\\n"\
-"    system(compile);\\n\\n"\
-"    char exec[20];\\n"\
-"    sprintf(exec, \\\"./Sully_%%d\\\", I - 1);\\n"\
-"    system(exec);\\n\\n"\
-"    return 0;\\n"\
-"}\\n"
 
 int main(void) {
     if (I <= 0)
         return 0;
 
-    char fname[20];
-    sprintf(fname, "Sully_%d.c", I - 1);
+    char filename[64];
+    char execname[64];
+    sprintf(filename, "sully_%d.c", I - 1);
+    sprintf(execname, "Sully_%d", I - 1);
 
-    FILE *f = fopen(fname, "w");
+    FILE *f = fopen(filename, "w");
     if (!f) return 1;
 
-    fprintf(f, CODE, I - 1, 34, CODE, 34);
+    char *code = "#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#include <unistd.h>%c%c#define I %d%c%cint main(void) {%c    if (I <= 0)%c        return 0;%c%c    char filename[64];%c    char execname[64];%c    sprintf(filename, %csully_%%d.c%c, I - 1);%c    sprintf(execname, %cSully_%%d%c, I - 1);%c%c    FILE *f = fopen(filename, %cw%c);%c    if (!f) return 1;%c%c    char *code = %c%s%c;%c    fprintf(f, code, 10,10,10,10,10,I-1,10,10,10,10,10,10,10,34,34,10,34,34,10,10,34,34,10,10,34,code,34,10);%c    fclose(f);%c%c    char cmd[128];%c    sprintf(cmd, %ccc -Wall -Wextra -Werror %%s -o %%s%c, filename, execname);%c    system(cmd);%c    if (I > 1)%c        execl(execname, execname, NULL);%c    return 0;%c}%c";
+    fprintf(f, code, 10,10,10,10,10,I-1,10,10,10,10,10,10,10,34,34,10,34,34,10,10,34,34,10,10,34,code,34,10);
+
     fclose(f);
 
-    char compile[100];
-    sprintf(compile, "clang -Wall -Wextra -Werror -o Sully_%d Sully_%d.c", I - 1, I - 1);
-    system(compile);
+    char cmd[128];
+    sprintf(cmd, "gcc -Wall -Wextra -Werror %s -o %s", filename, execname);
+    system(cmd);
 
-    char exec[20];
-    sprintf(exec, "./Sully_%d", I - 1);
-    system(exec);
+    if (I > 1)
+    {
+        char execpath[70];
+        snprintf(execpath, sizeof(execpath), "./%s", execname);
+        execl(execpath, execname, NULL);
+    }
 
     return 0;
 }
