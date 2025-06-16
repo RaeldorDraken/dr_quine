0dd0a1c97ad4c78ca0aaa9122f9645ef214095d9
Eduard Ros giralt <eros-gir@cbr5s3.42barcelona.com>
2025-06-16 18:02:41 +0200

    finnised fixing Grace tab issues

--- C/Grace.c
+++ C/Grace.c
@@ -7,8 +7,8 @@ int main(void) { \
     FILE *f = fopen(FILENAME, "w"); \
     if (!f) return (1); \
     fputs(GRACE_COMMENT, f); \
-    char *code = "%c#include <stdio.h>%c#define FILENAME %cGrace_kid.c%c%c#define GRACE_COMMENT %c/* Comentario obligatorio */%c%c#define GRACE() %c%cint main(void) {%c%cFILE *f = fopen(FILENAME, %cw%c);%c%c		if (!f) return (1);%c%c		fputs(GRACE_COMMENT, f);%c%c		char *code = %c%s%c;%c%c		fprintf(f, code, 10, 10, 34, 34, 10, 34, 34, 10, 92, 10, 92, 10, 34, 34, 92, 10, 92, 10, 92, 10, 34, code, 34, 92, 10, 92, 10, 92, 10, 92, 10, 10, 10);%c%c		fclose(f);%c%c		return (0);%c%c}%cGRACE()%c"; \
-    fprintf(f, code, 10, 10, 34, 34, 10, 34, 34, 10, 92, 10, 92, 10, 34, 34, 92, 10, 92, 10, 92, 10, 34, code, 34, 92, 10, 92, 10, 92, 10, 92, 10, 10, 10); \
+    char *code = "%c#include <stdio.h>%c#define FILENAME %cGrace_kid.c%c%c#define GRACE_COMMENT %c/* Comentario obligatorio */%c%c#define GRACE() %c%cint main(void) { %c%c    FILE *f = fopen(FILENAME, %cw%c); %c%c    if (!f) return (1); %c%c    fputs(GRACE_COMMENT, f); %c%c    char *code = %c%s%c; %c%c        fprintf(f, code, 10, 10, 34, 34, 10, 34, 34, 10, 92, 10, 92, 10, 34, 34, 92, 10, 92, 10, 92, 10, 34, code, 34, 92, 10, 92, 10, 92, 10, 92, 10, 10, 10); %c%c    fclose(f); %c%c    return (0); %c%c}%cGRACE()%c"; \
+        fprintf(f, code, 10, 10, 34, 34, 10, 34, 34, 10, 92, 10, 92, 10, 34, 34, 92, 10, 92, 10, 92, 10, 34, code, 34, 92, 10, 92, 10, 92, 10, 92, 10, 10, 10); \
     fclose(f); \
     return (0); \
 }
