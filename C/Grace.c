4f69e15e6f6517025042e66d2f10527033d8126d
Eduard Ros giralt <eros-gir@cbr5s3.42barcelona.com>
2025-06-16 17:50:02 +0200

    fixing diff (part 1 of 2) between Grace and Grace_kid

--- C/Grace.c
+++ C/Grace.c
@@ -1,5 +1,5 @@
+/* Comentario obligatorio */
 #include <stdio.h>
-
 #define FILENAME "Grace_kid.c"
 #define GRACE_COMMENT "/* Comentario obligatorio */"
 #define GRACE() \
@@ -7,23 +7,9 @@ int main(void) { \
     FILE *f = fopen(FILENAME, "w"); \
     if (!f) return (1); \
     fputs(GRACE_COMMENT, f); \
-    char *code = "%c#include <stdio.h>%c" \
-    "#define FILENAME %cGrace_kid.c%c%c" \
-    "#define GRACE_COMMENT %c/* Comentario obligatorio */%c%c" \
-    "#define GRACE() %c%c" \
-    "int main(void) { %c%c" \
-    "    FILE *f = fopen(FILENAME, %cw%c); %c%c" \
-    "    if (!f) return (1); %c%c" \
-    "    fputs(GRACE_COMMENT, f); %c%c" \
-    "    char *code = %c%s%c; %c%c" \
-    "        fprintf(f, code, 10, 10, 34, 34, 10, 34, 34, 10, 92, 10, 92, 10, 34, 34, 92, 10, 92, 10, 92, 10, 34, code, 34, 92, 10, 92, 10, 92, 10, 92, 10, 10, 10); %c%c" \
-    "    fclose(f); %c%c" \
-    "    return (0); %c%c" \
-    "}%c" \
-    "GRACE()%c"; \
+    char *code = "%c#include <stdio.h>%c#define FILENAME %cGrace_kid.c%c%c#define GRACE_COMMENT %c/* Comentario obligatorio */%c%c#define GRACE() %c%cint main(void) {%c%cFILE *f = fopen(FILENAME, %cw%c);%c%c		if (!f) return (1);%c%c		fputs(GRACE_COMMENT, f);%c%c		char *code = %c%s%c;%c%c		fprintf(f, code, 10, 10, 34, 34, 10, 34, 34, 10, 92, 10, 92, 10, 34, 34, 92, 10, 92, 10, 92, 10, 34, code, 34, 92, 10, 92, 10, 92, 10, 92, 10, 10, 10);%c%c		fclose(f);%c%c		return (0);%c%c}%cGRACE()%c"; \
     fprintf(f, code, 10, 10, 34, 34, 10, 34, 34, 10, 92, 10, 92, 10, 34, 34, 92, 10, 92, 10, 92, 10, 34, code, 34, 92, 10, 92, 10, 92, 10, 92, 10, 10, 10); \
     fclose(f); \
     return (0); \
 }
-
 GRACE()
